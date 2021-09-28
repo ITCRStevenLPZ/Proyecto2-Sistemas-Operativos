203d15637f65541569ca94023d4d3cdcc1040f50
ITCRStevenLPZ <esquivellopezronald@hotmail.com>
2021-09-28 00:34:16 -0600

    [+]Se agrega la funcion que hace el sorteo de lottery y se agrega una funcion de prueba

--- corrutinas.c
+++ corrutinas.c
@@ -3,8 +3,6 @@
 
 int contador_id = 0; // variable global que sirve para asignar los ids diferentes a cada hilo.
 
-int tiquetes_totales = 0; //tiquetes totales en toda la ejecucion, basicamente es la suma de todos los tiquetes individuales
-
 void agregar_hilo(hilo* nuevo_hilo){ //funcion encargada de insertar un nuevo hilo a la lista de hilos
 
     if(primero_lottery == NULL){
@@ -51,9 +49,34 @@ void agregar_lottery(hilo* nuevo_hilo){ //funcion encargada de agregar el hilo a
 }
 
 
+hilo* sorteo(){ //funcion encargada de sortear el siguiente hilo a entrar al CPU. Es una implementacion simple de Lottery Shecduling
+
+    int resultado_sorteo = (rand() % (tiquetes_totales));
+
+    hilo* actual = primero_lottery;
+
+    if(actual == NULL){
+        printf("SORTEO - NO HAY HILOS EN LA LISTA\n");
+        return NULL;
+    }else{
+        printf("SORTEO - SE REALIZA EL SORTEO\n");
+        int tiquetes_de_actual = actual->tiquetes;
+
+        while(tiquetes_de_actual <= resultado_sorteo){ //este while determina el hilo ganador
+            actual = actual->siguiente;
+            tiquetes_de_actual += actual->tiquetes;
+        }
+        return actual;
+    }
+
+
+}
+
+
+
 void crear_hilo(void* funcion, void* param, int tiquetes){ //funcion encargada de crear el hilo y asignarle sus atributos
     hilo* nodo =(hilo*)malloc(sizeof(hilo));
-
+    nodo->estado = 1; //el hilo esta listo
     nodo->id = contador_id++;
     nodo->tiquetes = tiquetes;
     nodo->funcion = funcion;
@@ -68,40 +91,36 @@ void crear_hilo(void* funcion, void* param, int tiquetes){ //funcion encargada d
 
 void administrador_hilos(){ //funcion encargada de administrar los hilos, decide, junto con lottery, cuales hilos van al CPU
 
-    //The last thread that used the CPU is marked as ready
+    printf("ADMIN - ENTRO\n");
     if(en_CPU != NULL && en_CPU->estado == 2){
         en_CPU->estado = 1;
     }
 
     if(primero_lottery == NULL){ //cuando no hay mas hilos, termina
+        printf("ADMIN - NO HAY HILOS EN LA LISTA\n");
         en_CPU = NULL;
         exit(0);
     }
     else{
-        //en_CPU = sorteo(lottery_threads, ticket_amount);
+        en_CPU = sorteo(); //se sortea el hilo que va a ir al CPU
     }
 
-    //If the selected thread is no ready to run, calculates a new thread
+    
     if(en_CPU->estado != 1){
-
         administrador_hilos();
 
     }else{
-
-        en_CPU->estado = 2;
+        
+        en_CPU->estado = 2; //el
 
         if (sigsetjmp(buffer_administrador,1) != 0) { //quiere decir que el thread en el CPU se pauso o termino su ejecucion
             administrador_hilos();
         }
 
-        //If the thread has not started its execution
         if(en_CPU->corriendo == 0){
             en_CPU->corriendo = 1;
             (*en_CPU->funcion)(en_CPU->param);
-        }
-
-        //When the thread context has to be restored
-        else{
+        }else{
             siglongjmp(en_CPU->buffer_hilo,1);
         }
     }
@@ -112,20 +131,13 @@ void finalizar_hilo(){ //funcion encargada de eliminar el hilo, aya que este ya 
     if(en_CPU != NULL){
 
         tiquetes_totales -= en_CPU->tiquetes;
+        printf("FINALIZAR - SE ELIMINA EL HILO CON ID %d\n", en_CPU->id);
         borrar_hilo(en_CPU);
-
         if(en_CPU->desechado == 1){
             free(en_CPU);
         }
 
         siglongjmp(buffer_administrador, 1);
-    }
-}
-
-void pausar_hilo(){ //funcion que se encarga de guardar el estado del hilo, y de ceder el procesador al hilo del administrador de hilos
-
-    if(sigsetjmp(en_CPU->buffer_hilo,1) == 0){
-        siglongjmp(buffer_administrador,1);
     }
 }
 
@@ -135,20 +147,42 @@ void prueba(void* M){ //funcion de prueba que va a recorrer el hilo
     long double sum = 0;
     int i;
     for (i = 1; i <= m; i++){
-        if(i+1 % QUANTUM == 0){
-            pausar_hilo();
+        if(i % QUANTUM == 0){
+            printf("Iteracion = %d - Resultado por ahora = %Lf \n", i, sum);
+            en_CPU->i = i;
+            en_CPU->sum = sum; //se guarda el entorno local
+            en_CPU->m = m;
+            if(sigsetjmp(en_CPU->buffer_hilo,1) == 0){
+                siglongjmp(buffer_administrador,1);      // se pausa el hilo
+            }
+            i = en_CPU->i;
+            sum = en_CPU->sum; //se asigna en el entorno local, el que se guardo antes de que se pusara
+            m = en_CPU->m;
         }
         long double pi = (1/(double)pow(i, 2));
         sum += pi;
-        //printf("%d - %Lf \n", i, sum);
     }
+    en_CPU->desechado = 1; //se desecha el hilo, no hay necesidad de usarlo despues.
     finalizar_hilo();
 }
 
 int main(){
     srand(time(NULL));
+    tiquetes_totales = 0;
+    en_CPU = NULL;
+
     int* M = (int*)malloc(sizeof(int));
-    *M = 60000;
+    *M = 1000000;
     crear_hilo(&prueba, (void*) M, 50);
+
+    int* M2 = (int*)malloc(sizeof(int));
+    *M2 = 1000000;
+    crear_hilo(&prueba, (void*) M2, 75);
+
+    int* M3 = (int*)malloc(sizeof(int));
+    *M2 = 100000;
+    crear_hilo(&prueba, (void*) M3, 150);
+
+    administrador_hilos();
     return 0;
     }
--- corrutinas.h
+++ corrutinas.h
@@ -7,10 +7,13 @@
 #include <math.h>
 
 #define MAX_HILOS 25 //maxima cantidad de hilos creados (requerimiento del proyecto)
-#define QUANTUM 100 //cantidad de iteraciones por turno
+#define QUANTUM 100000 //cantidad de iteraciones por turno
 
 typedef struct hilo{ //Struct que respresenta un hilo del scheduler
     int id; //identificador del hilo
+    long double sum; //numero de la suma que ha ido adicionando
+    int i; //numero de la iteracion que va corriendo
+    int m; //numero de iteraciones limite
     int tiquetes; //tiquetes del hilo (Para Lottery Scheduling)
     int desechado; //flag que indica que el hilo ya termino su trabajo, y debe ser eliminado
     int corriendo; //flag que indica que el hilo ya se ha ejecutado
@@ -29,10 +32,12 @@ hilo* en_CPU; //hilo que se va a encontrar en el CPU
 
 sigjmp_buf buffer_administrador; //buffer del hilo que administra los hilos
 
+int tiquetes_totales; //tiquetes totales en toda la ejecucion, basicamente es la suma de todos los tiquetes de los hilos individuales
+
 void agregar_hilo(hilo* nuevo_hilo); //funcion encargada de insertar un nuevo hilo a la lista de hilos
 void crear_hilo(void* funcion, void* param, int tiquetes); //funcion encargada de crear el hilo y asignarle sus atributos
 void borrar_hilo(hilo* a_borrar); //funcion encargada de borrar un hilo de la lista de hilos
 void crear_hilo(void* funcion, void* param, int tiquetes); //funcion encargada de crear el hilo y asignarle sus atributos
 void administrador_hilos(); //funcion encargada de administrar los hilos, decide, junto con lottery, cuales hilos van al CPU
 void finalizar_hilo(); //funcion encargada de eliminar el hilo, aya que este ya termino con sus funciones
-void pausar_hilo(); //funcion que se encarga de guardar el estado del hilo, y de ceder el procesador al hilo del administrador de hilos
+hilo* sorteo(); //funcion encargada de sortear el siguiente hilo a entrar al CPU. Es una implementacion simple de Lottery Shecduling
